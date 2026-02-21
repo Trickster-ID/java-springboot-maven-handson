54d138dbc41cd6ccb28e4548bfe76435dad632dc
Pikri <pikritaufanaziz@gmail.com>
2026-02-22 01:04:16 +0700

    complete ping api with updated docker compose and makefile

--- src/main/java/pik/restful/controllers/PingController.java
+++ src/main/java/pik/restful/controllers/PingController.java
@@ -1,4 +1,13 @@
 package pik.restful.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
 }
